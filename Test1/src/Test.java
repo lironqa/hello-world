5b9713378e1d68572d0b7181d2d0f2128d869c88
lironqa <contact@lironqa.com>
2016-01-25 22:36:58 +0200

    Adding input/output

--- Test1/src/Test.java
+++ Test1/src/Test.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
+		String username;
+		Scanner reader = new Scanner (System.in);
 		System.out.println("Hello world");
+		System.out.println("What is your name?");
+		username=reader.next();
+		System.out.println("Hello, "+username);
+		reader.close();
 	}
 
 }
